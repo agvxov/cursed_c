99730c106c0e13be4f9b9524c74f358fd73dcec9
anon <anon@anon.anon>
2023-09-25 00:42:03 +0200

    init

---
+++ c.c
@@ -0,0 +1,34 @@
+??=define ARGUMENT_PREFIX(x)  $arg %:%: x
+
+typedef unsigned char string;
+
+ű(_Decimal32 named_argument, ...) ??<
+  int $a<:0:>;
+  puts("Let me die!");
+  return -1[$a];
+??>
+
+//fortran /* sadly gcc doesn't support it */
+_Noreturn
+const void
+main
+(
+  ARGUMENT_PREFIX (c),
+  ARGUMENT_PREFIX (v),
+  $envp
+)
+  __int128 ARGUMENT_PREFIX (c);
+  string*    ARGUMENT_PREFIX (v);
+  string**   $envp;
+??<
+  signed f
+  (
+  )
+  ??<
+    ((void (*)(void))ű)();
+  ??>
+; puts("Make it stop, cruel world!")
+, puts("//Please!" + 2)
+, f()
+;
+??>
